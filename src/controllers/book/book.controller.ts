ddc38f64fa947470723616d2357d89a9ff2e933f
Caique Daniel <caiquedaniel8@gmail.com>
2022-12-07 20:09:18 -0300

    Rota de edição e recuperação de livros.

--- src/controllers/book/book.controller.ts
+++ src/controllers/book/book.controller.ts
@@ -1,5 +1,4 @@
-import {Body, Controller, Header, Post, Res} from '@nestjs/common';
-import {Request} from 'express';
+import {Body, Controller, Get, Header, Param, Post, Put, Query, Res} from '@nestjs/common';
 import BookDto from "./dtos/book.dto";
 import BookService from "./book.service";
 
@@ -12,5 +11,23 @@ export class BookController {
     @Header('Access-Control-Allow-Origin', '*')
     public async create(@Body() bookDto: BookDto) {
         await this.service.create(bookDto);
+    }
+
+    @Get()
+    @Header('Access-Control-Allow-Origin', '*')
+    public async getAll(@Query('page') page) {
+        return await this.service.getAll(page ?? 1);
+    }
+
+    @Get(':id')
+    @Header('Access-Control-Allow-Origin', '*')
+    public async findOne(@Param('id') id) {
+        return await this.service.findOne(id);
+    }
+
+    @Put(':id')
+    @Header('Access-Control-Allow-Origin', '*')
+    public async edit(@Param('id') id, @Body() bookDto: BookDto) {
+        return await this.service.edit(id, bookDto);
     }
 }
--- src/controllers/book/book.service.ts
+++ src/controllers/book/book.service.ts
@@ -4,6 +4,7 @@ import {EntityManager} from "@mikro-orm/mysql";
 import Book from "../../entities/Book";
 import BookDto from "./dtos/book.dto";
 import Author from "../../entities/Author";
+import Contracts from "../../contracts/Contracts";
 
 @Injectable()
 class BookService {
@@ -24,6 +25,47 @@ class BookService {
             bookDto.price,
             bookDto.active
         );
+
+        await this.em.persistAndFlush(book);
+    }
+
+    public async getAll(page: number): Promise<Contracts.Pagination<Book>> {
+        page = page < 1 ? 1 : page;
+
+        const offset = (page - 1) * Book.LIMIT, total = await this.em.count(Book);
+        const books = await this.em.find(Book, {}, {offset, limit: Book.LIMIT});
+
+        return {
+            data: books,
+            next: offset + Book.LIMIT < total ? `/api/books?page=${++page}` : null
+        };
+    }
+
+    public async findOne(id: number): Promise<Book> {
+        const book = await this.em.findOne(Book, {id});
+
+        if (!book)
+            throw new HttpException("Not Found", HttpStatus.NOT_FOUND);
+
+        return book;
+    }
+
+    public async edit(id: number, bookDto: BookDto) {
+        const promises = [
+            this.findOne(id),
+            this.em.findOne(Author, {id: bookDto.author}),
+        ];
+
+        let [book, author] = await Promise.all(promises);
+
+        book = (book as Book);
+
+        book.setName(bookDto.name);
+        book.setEdition(bookDto.edition);
+        book.setPrice(bookDto.price);
+        book.setPages(bookDto.pages);
+        book.setActive(bookDto.active);
+        book.setAuthor(author as Author);
 
         await this.em.persistAndFlush(book);
     }
